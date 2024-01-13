d70efe4c6bb2cb89cb6a0ce81e6b8044ec93a871
Huyh Huyhu <2509roblox@gmail.com>
2024-01-13 18:00:20 +0700

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,225 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+
+use Illuminate\Routing\Controller as BaseController;
+
+use App\Models\Tasks;
+use App\Models\Roles;
+use App\Models\User;
+use App\Models\Projects;
+use App\Models\TaskComment;
+use Carbon\Carbon;
+
+class TaskController extends BaseController
+{
+	public function index()
+	{
+		$user = Auth::user();
+
+		$data['arRoles'] = Roles::whereIn('code', [User::ADMIN, User::MANAGER])->get()->pluck('name', 'id')->toArray();
+		$data['arTasks'] = Tasks::with('tasksAssignTo')->with('tasksCreatedBy')->with('tasksApprovedBy')->where('parent_id', '=', 0)->get()->keyBy('id')->toArray();
+
+		foreach ($data['arTasks'] as $taskId => $task) {
+			// Kiểm tra xem có nhiệm vụ con với parent_id = task.id hay không
+			$hasChildren = Tasks::where('parent_id', '=', $taskId)->exists();
+
+			// Gán giá trị biến kiểm tra `hasChildren` vào nhiệm vụ hiện tại
+			$data['arTasks'][$taskId]['hasChildren'] = $hasChildren;
+
+			// Calculate the level of the current task
+			$level = 0;
+			$parentId = $task['parent_id'];
+			while ($parentId != 0) {
+				$level++;
+				$parentId = Tasks::where('id', $parentId)->value('parent_id');
+			}
+			$data['arTasks'][$taskId]['level'] = $level;
+			$avatarPath = '/assets/images/users/avatar-' . $data['arTasks'][$taskId]['assign_to'] . '.jpg';
+
+			if (file_exists(public_path($avatarPath))) {
+				$data['arTasks'][$taskId]['avatar'] = '/assets/images/users/avatar-' . $data['arTasks'][$taskId]['assign_to']  . '.jpg';
+			} else {
+				$data['arTasks'][$taskId]['avatar'] = '/assets/images/users/avatar-basic..jpg';
+				// Đường dẫn ảnh không tồn tại
+				// Xử lý tương ứng tại đây
+			}
+		}
+
+		$data['arProject'] = Projects::get()->pluck('name', 'id')->toArray();
+		$data['user_id'] = $user->id;
+
+		return view('tasks.index', $data);
+	}
+
+	public function detail(Request $request)
+	{
+		$user = Auth::user();
+
+		$data = array();
+
+		$data['task'] = Tasks::with('tasksAssignTo')->with('tasksCreatedBy')->with('tasksApprovedBy')->where('id', '=', $request->id)->get()->toArray();
+
+		$data['arChildTasks'] = Tasks::with('tasksAssignTo')->with('tasksCreatedBy')->with('tasksApprovedBy')->where('parent_id', '=', $request->id)->get()->toArray();
+
+		$data['arStatus'] = Tasks::STATUS;
+		$data['arPriority'] = Tasks::PRIORITY;
+		$data['task_id'] = $request->id;
+
+		$data['taskComments'] = TaskComment::where('task_id', '=', $request->id)->get();
+
+		$data['user_id'] =  Auth::user()->id;
+
+		$data['taskComments'] = TaskComment::with('user')
+			->where('task_id', $request->id)
+			->where('reply_id', 0)
+			->orderBy('created_at', 'desc')
+			->take(3)
+			->get();
+
+		// Chuyển đổi múi giờ và định dạng thời gian cho mỗi TaskComment
+		foreach ($data['taskComments'] as $taskComment) {
+			$createdDate = \Carbon\Carbon::parse($taskComment->created_at)->setTimezone('Asia/Ho_Chi_Minh');
+			$taskComment->diffForHumansInVietnam = $createdDate->diffForHumans();
+		}
+
+		$data['taskCommentsWithReplyCount'] = [];
+
+		foreach ($data['taskComments'] as $taskComment) {
+			$taskComment->replyCount = TaskComment::where('reply_id', $taskComment->id)->count();
+			$data['taskCommentsWithReplyCount'][] = $taskComment;
+		}
+		// Chuyển đổi múi giờ và định dạng thời gian cho mỗi TaskComment có reply_id khác 0
+
+		// Chuyển đổi múi giờ và định dạng thời gian cho mỗi TaskComment có reply_id khác 0
+
+
+		$data['taskCommentss'] = TaskComment::where('task_id', '=', $request->id)->get();
+
+		$data['commentCount'] = count($data['taskCommentss']);
+		return view('tasks.detail', $data);
+	}
+
+
+
+	public function add(Request $request)
+	{
+		$data['users'] = User::get()->pluck('name', 'id')->toArray();
+
+		$data['parentId'] = $request->parent_id;
+
+		$data['parentTask'] = array();
+		$data['taskCode'] = '';
+
+		if ($data['parentId'] != 0) {
+			$data['parentTask'] = Tasks::with('tasksAssignTo')->with('tasksCreatedBy')->with('tasksApprovedBy')->where('id', '=', $data['parentId'])->get()->toArray();
+		}
+
+		$data['arProject'] = Projects::get()->pluck('name', 'id')->toArray();
+
+		// dd($data);
+
+		return view('tasks.add', $data);
+	}
+
+	public function edit(Request $request)
+	{
+		$data['task'] = Tasks::with('tasksAssignTo')->with('tasksCreatedBy')->with('tasksApprovedBy')->where('id', '=', $request->id)->get()->toArray();
+
+		$data['users'] = User::get()->pluck('name', 'id')->toArray();
+
+		$data['arStatus'] = Tasks::STATUS;
+		$data['arPriority'] = Tasks::PRIORITY;
+
+		return view('tasks.edit', $data);
+	}
+
+	public function update(Request $request)
+	{
+		if (empty($request->task['id'])) {
+			$taskId = Tasks::create(array(
+				"project_id" => $request->task["project_id"],
+				"parent_id" => $request->task["parent_id"],
+				"name" => $request->task["name"],
+				"description" => $request->task["description"],
+				"assign_to" => $request->task["assign_to"],
+				"approved_by" => $request->task["approved_by"],
+				"due_date" => $request->task["due_date"],
+				"task_value" => $request->task["task_value"],
+				"priority" => $request->task["priority"],
+				"status" => $request->task["status"],
+			))->id;
+		} else {
+			$taskId = $request->task["id"];
+			$task = Tasks::find($request->task["id"]);
+
+			$task->name = $request->task["name"];
+			$task->description = $request->task["description"];
+			$task->assign_to = $request->task["assign_to"];
+			$task->approved_by = $request->task["approved_by"];
+			$task->task_value = $request->task["task_value"];
+			$task->due_date = $request->task["due_date"];
+			$task->priority = $request->task["priority"];
+			$task->status = $request->task["status"];
+
+			$task->save();
+		}
+
+		return redirect()->route('task.detail', ['id' => $taskId]);
+	}
+	public function get_child_tasks(Request $request)
+	{
+		$id = $request->input('id');
+		$arTasks = Tasks::with('tasksAssignTo')
+			->with('tasksCreatedBy')
+			->with('tasksApprovedBy')
+			->where('parent_id', $id)
+			->get()
+			->keyBy('id')
+			->toArray();
+
+		foreach ($arTasks as $taskId => $task) {
+			// Check if there are child tasks with parent_id = $taskId
+			$hasChildren = Tasks::where('parent_id', $taskId)->exists();
+
+			// Assign the value of `hasChildren` to the current task
+			$arTasks[$taskId]['hasChildren'] = $hasChildren;
+
+			// Calculate the level of the current task
+			$level = 0;
+			$parentId = $task['parent_id'];
+			$parentIds = [];
+			while ($parentId != 0) {
+				$level++;
+				$parentTask = Tasks::find($parentId);
+				if ($parentTask) {
+					$parentId = $parentTask->parent_id;
+					$parentIds[] = $parentId;
+				} else {
+					break;
+				}
+			}
+			$arTasks[$taskId]['level'] = $level;
+			$arTasks[$taskId]['parentIds'] = array_reverse($parentIds);
+			$avatarPath = '/assets/images/users/avatar-' . $task['assign_to'] . '.jpg';
+
+			if (file_exists(public_path($avatarPath))) {
+				$arTasks[$taskId]['avatar'] = '/assets/images/users/avatar-' . $task['assign_to'] . '.jpg';
+			} else {
+				$arTasks[$taskId]['avatar'] = '/assets/images/users/avatar-basic.jpg';
+				// Handle the case when the image path does not exist
+			}
+		}
+
+		$html = view('tasks.render_child_tasks', ['arTasks' => $arTasks])->render();
+
+		return response()->json(['html' => $html], 200);
+	}
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use App\Models\Roles;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends BaseController {
+	public function loginForm() {
+		if (Auth::check()) {
+			return redirect(route('dashboard'));
+		}
+		
+		return view('users.login-form');
+	}
+
+	public function index() {
+		$data['arUsers'] = User::with('userRole')->get()->toArray();
+		$users = User::with('userRole')->get();
+
+		foreach($users as $user){
+			$data['avatarUsers'][$user->id] = User::find($user->id)->getMedia('avatar');
+		}
+
+		// dd($data);
+
+		return view('users.index', $data);
+	}
+
+	public function add() {
+		$data['arRole'] = Roles::get()->pluck('name', 'id')->toArray();
+		return view('users.add', $data);
+	}
+
+	public function edit(Request $request) {
+		$data['user'] = User::with('userRole')->where('id', '=', $request->id)->get()->toArray();
+		$data['arRole'] = Roles::get()->pluck('name', 'id')->toArray();
+		$data['avatarUsers'][$request->id] = User::find($request->id)->getMedia('avatar');
+		return view('users.edit', $data);
+	}
+
+	public function login(Request $request) {
+		if (Auth::check()) {
+			return redirect(route('dashboard'));
+		}
+
+		if (Auth::attempt(array(
+			'email' => $request->input('email'),
+			'password' => $request->input('password'),
+		), $request->input('remember'))) {
+			return redirect(route('dashboard'));
+		}
+
+		return redirect()->back();
+	}
+
+	public function detail(Request $request) {
+		return view('users.detail');
+	}
+
+	public function update(Request $request) {
+		if (empty($request->id)) {
+			$request->validate([
+				'name' => 'required',
+				'password' => 'required',
+				'email' => 'required'
+			]);
+
+			$id = User::create(array(
+				"name" => $request->name,
+				"password" => Hash::make($request->password),
+				"email" => $request->email,
+				"role_id" => $request->role_id,
+				"country" => $request->country,
+				"phone" => $request->phone,
+				"point_accumulated" => $request->point_accumulated,
+				"expertise_coefficient" => $request->expertise_coefficient,
+				"allowance" => $request->allowance
+			))->id;
+
+			if (isset($request->avatar) && !empty($request->avatar)){
+				$user = User::findOrFail($id);
+				$user->addMedia($request->avatar)->toMediaCollection('avatar');
+			}
+		} else {
+			$user = User::find($request->id);
+			
+			$user->email = $request->email;
+			$user->name = $request->name;
+			$user->role_id = $request->role_id;
+			$user->country = $request->country;
+			$user->phone = $request->phone;
+			$user->point_accumulated = $request->point_accumulated;
+			$user->expertise_coefficient = $request->expertise_coefficient;
+			$user->allowance = $request->allowance;
+
+			$user->save();
+
+			if(isset($request->avatar) && !empty($request->avatar)){
+				$user->clearMediaCollection('avatar');
+				$user->addMedia($request->avatar)->toMediaCollection('avatar');
+			}
+		}
+
+		return redirect()->route('user.index');
+
+	}
+}
